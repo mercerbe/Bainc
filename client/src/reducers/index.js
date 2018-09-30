5b88ddd35a9b652156fc9b6a7b6af384cd706ee5
Admin <mercerbe@gmail.com>
2018-09-30 16:00:25 -0400

    set up post reducer and actions

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -3,10 +3,12 @@ import { combineReducers } from 'redux'
 import authReducer from './authReducer'
 import errorReducer from './errorReducer'
 import profileReducer from './profileReducer'
+import postReducer from './postReducer'
 
 //export object of reducers with combine method
 export default combineReducers({
   auth: authReducer,
   errors: errorReducer,
-  profile: profileReducer
+  profile: profileReducer,
+  post: postReducer
 })
