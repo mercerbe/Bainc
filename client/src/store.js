a86687545b11b8bb8d9e3eb7dcecb828d55e9879
Admin <mercerbe@gmail.com>
2018-10-02 15:10:25 -0400

    update store options

--- client/src/store.js
+++ client/src/store.js
@@ -12,14 +12,22 @@ const initialState = {}
 const middleware = [thunk]
 
 //set store -- reducer array--rootReducer, state obj, middleware with spread
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 const store = createStore(
   rootReducer,
   initialState,
-  compose(
-    applyMiddleware(...middleware),
-    //add redux extension 
-    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composeEnhancers(
+    applyMiddleware(...middleware)
   )
 )
+// const store = createStore(
+//   rootReducer,
+//   initialState,
+//   compose(
+//     applyMiddleware(...middleware),
+//     //add redux extension
+//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+//   )
+// )
 
 export default store
