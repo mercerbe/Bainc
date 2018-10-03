a3bba9025530b8357c856f240415476178cc3431
Admin <mercerbe@gmail.com>
2018-10-03 12:19:26 -0400

    path resolve for delpoyment

--- server.js
+++ server.js
@@ -55,7 +55,7 @@ if(process.env.NODE_ENV === 'production') {
 
   //routes
   app.get('*', (req, res) => {
-    res.sendFile(path.join(__dirname+'/client/build/index.html'))
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
   })
 }
 //===============================================//
